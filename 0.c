ecfc04311f4b1470f172166be590a0f006e7d4a2
iskandaraliens <monishangvip@gmail.com>
2020-06-29 17:16:09 +0800

    first commit for init

--- 0.c
+++ 0.c
@@ -1,5 +1,6 @@
-#include 'stdio.h'
-main()
+#include <stdio.h>
+int main()
 {
-	printf("hello world !");
+printf("Hellow World!");
+return 0;
 }
---
+++ 1.c
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace  std;
+int main()
+{
+	cout << "WELL DONE" <<endl;
+	return 0;
+}
